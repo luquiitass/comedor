094a42aa679b807e3918bf9d9a06c10575c8984e
Lucas Larrea <liquiitass@gmail.com>
2016-08-02 04:01:07 -0300

    inicio de la implementacion del modulo de asistencia y faltas por parte del administrador

---
+++ vendor/amranidev/ajaxis/resources/views/bootstrap/types/textArea.blade.php
@@ -0,0 +1,4 @@
+<div class="form-group">
+<label class="control-label">{{$label}}</label>
+<textarea class="form-control" cols="20" style="width:100%; height: 69px;" name="{{$name}}" rows="10" id="{{$name}}" placeholder = "{{$label}}">{{$value}}</textarea>
+</div>
