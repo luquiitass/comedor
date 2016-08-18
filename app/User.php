8cc34d3cabffff3724f74f15b33d2d4f76c9b75e
Lucas Larrea <liquiitass@gmail.com>
2016-08-18 14:49:43 -0300

    Solucionando el temita de la imagen

--- app/User.php
+++ app/User.php
@@ -40,7 +40,7 @@ class User extends Authenticatable
 
     public function getImagen()
     {
-        if ( file_exists(public_path().'storage/'.$this->id) ) {
+        if ( file_exists(public_path().'storage/'.$this->imagen) ) {
             return public_path().'/storage/'.$this->imagen;
         }
         return public_path().'storage/login2.png'; 
